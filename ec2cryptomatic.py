2fa299565ab24887fa1bed8ed7dc4b46c86844cd
Julien B <julien@toshokan.fr>
2019-08-28 17:19:31 +0200

    Refactoring code and removing the need of intermediate snapshot for encryption

--- ec2cryptomatic.py
+++ ec2cryptomatic.py
@@ -21,10 +21,11 @@ class EC2Cryptomatic(object):
     """ Encrypt EBS volumes from an EC2 instance """
 
     def __init__(self, region: str, instance: str, key: str):
-        """ Constructor
-            :param region: the AWS region where the instance is
-            :param instance: one instance-id
-            :param key: the AWS KMS Key to be used to encrypt the volume
+        """
+        Constructor
+        :param region: the AWS region where the instance is
+        :param instance: one instance-id
+        :param key: the AWS KMS Key to be used to encrypt the volume
         """
         self._logger = logging.getLogger('ec2-cryptomatic')
         self._logger.setLevel(logging.DEBUG)
@@ -37,7 +38,6 @@ class EC2Cryptomatic(object):
 
         # Volumes
         self._snapshot = None
-        self._encrypted = None
         self._volume = None
 
         # Waiters
@@ -58,44 +58,49 @@ class EC2Cryptomatic(object):
         if self._instance.state['Name'] != 'stopped':
             raise TypeError('Instance still running ! please stop it.')
 
-
     def _start_instance(self):
         try:
-            self._logger.info('-> Starting instance %s' % self._instance.id)
+            self._logger.info('-- Starting instance %s' % self._instance.id)
             self._ec2_client.start_instances(InstanceIds=[self._instance.id])
-            self._logger.info('-> Instance %s started' % self._instance.id)
+            self._logger.info('-- Instance %s started' % self._instance.id)
         except ClientError:
             raise
 
     def _cleanup(self, device, discard_source):
-        """ Delete the temporary objects
-            :param device: the original device to delete
+        """
+        Delete the temporary objects
+        :param device: the original device to delete
         """
 
-        self._logger.info('->Cleanup of resources')
+        self._logger.info('-- Cleanup of resources')
         self._wait_volume.wait(VolumeIds=[device.id])
 
         if discard_source:
-            self._logger.info('-->Deleting unencrypted volume %s' % device.id)
+            self._logger.info('--- Deleting unencrypted volume %s' % device.id)
             device.delete()
 
         else:
-            self._logger.info('-->Preserving unencrypted volume %s' % device.id)
+            self._logger.info('--- Preserving unencrypted volume %s' % device.id)
 
         self._snapshot.delete()
-        self._encrypted.delete()
 
     def _create_volume(self, snapshot, original_device):
-        """ Create an encrypted volume from an encrypted snapshot
-            :param snapshot: an encrypted snapshot
-            :param original_device: device where take additionnal informations
         """
+        Create an encrypted volume from an encrypted snapshot
+        :param snapshot: an encrypted snapshot
+        :param original_device: device where take additional information
+        """
+        vol_args = {'SnapshotId': snapshot.id,
+                    'VolumeType': original_device.volume_type,
+                    'AvailabilityZone': original_device.availability_zone,
+                    'Encrypted': True,
+                    'KmsKeyId': self._kms_key}
 
-        self._logger.info('->Creating an encrypted volume from %s' % snapshot.id)
-        volume = self._ec2_resource.create_volume(
-                            SnapshotId=snapshot.id,
-                            VolumeType=original_device.volume_type,
-                            AvailabilityZone=original_device.availability_zone)
+        if original_device.volume_type.startswith('io'):
+            vol_args['Iops'] = original_device.iops
+
+        self._logger.info('-- Creating an encrypted volume from %s' % snapshot.id)
+        volume = self._ec2_resource.create_volume(**vol_args)
         self._wait_volume.wait(VolumeIds=[volume.id])
 
         if original_device.tags:
@@ -103,36 +108,26 @@ class EC2Cryptomatic(object):
 
         return volume
 
-    def _encrypt_snapshot(self, snapshot):
-        """ Copy and encrypt a snapshot
-            :param snapshot: snapshot to copy
-        """
-
-        self._logger.info('->Copy the snapshot %s and encrypt it' % snapshot.id)
-        snap_id = snapshot.copy(Description='encrypted copy of %s' % snapshot.id,
-                                Encrypted=True, SourceRegion=self._region, KmsKeyId=self._kms_key)
-        snapshot = self._ec2_resource.Snapshot(snap_id['SnapshotId'])
-        self._wait_snapshot.wait(SnapshotIds=[snapshot.id])
-        return snapshot
-
     def _swap_device(self, old_volume, new_volume):
-        """ Swap the old device with the new encrypted one
-            :param old_volume: volume to detach from the instance
-            :param new_volume: volume to attach to the instance
+        """
+        Swap the old device with the new encrypted one
+        :param old_volume: volume to detach from the instance
+        :param new_volume: volume to attach to the instance
         """
 
-        self._logger.info('->Swap the old volume and the new one')
+        self._logger.info('-- Swap the old volume and the new one')
         device = old_volume.attachments[0]['Device']
         self._instance.detach_volume(Device=device, VolumeId=old_volume.id)
         self._wait_volume.wait(VolumeIds=[old_volume.id])
         self._instance.attach_volume(Device=device, VolumeId=new_volume.id)
 
     def _take_snapshot(self, device):
-        """ Take the first snapshot from the volume to encrypt
-            :param device: EBS device to encrypt
+        """
+        Take the first snapshot from the volume to encrypt
+        :param device: EBS device to encrypt
         """
 
-        self._logger.info('->Take a first snapshot for volume %s' % device.id)
+        self._logger.info('-- Take a snapshot for volume %s' % device.id)
         snapshot = device.create_snapshot(Description='snap of %s' % device.id)
         self._wait_snapshot.wait(SnapshotIds=[snapshot.id])
         return snapshot
@@ -140,7 +135,7 @@ class EC2Cryptomatic(object):
     def start_encryption(self, discard_source):
         """ Launch encryption process """
 
-        self._logger.info('Start to encrypt instance %s' % self._instance.id)
+        self._logger.info('\nStart to encrypt instance %s\n' % self._instance.id)
 
         # We encrypt only EC2 EBS-backed. Support of instance store will be
         # added later
@@ -158,7 +153,7 @@ class EC2Cryptomatic(object):
                 self._logger.warning(msg)
                 continue
 
-            self._logger.info('>Let\'s encrypt volume %s ' % device.id)
+            self._logger.info('- Let\'s encrypt volume %s ' % device.id)
 
             # Keep in mind if DeleteOnTermination is need
             delete_flag = device.attachments[0]['DeleteOnTermination']
@@ -167,10 +162,8 @@ class EC2Cryptomatic(object):
 
             # First we have to take a snapshot from the original device
             self._snapshot = self._take_snapshot(device)
-            # Then, copy this snapshot and encrypt it
-            self._encrypted = self._encrypt_snapshot(self._snapshot)
-            # Create a new volume from that encrypted snapshot
-            self._volume = self._create_volume(self._encrypted, device)
+            # Create a new encrypted volume from that snapshot
+            self._volume = self._create_volume(self._snapshot, device)
             # Finally, swap the old-device for the new one
             self._swap_device(device, self._volume)
             # It's time to tidy up !
@@ -178,7 +171,7 @@ class EC2Cryptomatic(object):
             # starting the stopped instance
              
             if not discard_source:
-                self._logger.info('>Tagging legacy volume %s with replacement '
+                self._logger.info('- Tagging legacy volume %s with replacement '
                                   'id %s' % (device.id, self._volume.id))
                 device.create_tags(
                     Tags=[
@@ -190,18 +183,24 @@ class EC2Cryptomatic(object):
                 )
 
             if delete_flag:
-                self._logger.info('->Put flag DeleteOnTermination on volume')
+                self._logger.info('-- Put flag DeleteOnTermination on volume')
                 self._instance.modify_attribute(BlockDeviceMappings=[flag_on])
         self._start_instance()
         self._logger.info('End of work on instance %s\n' % self._instance.id)
 
 
-def main(arguments):
-    """ Start the main program """
+def main(args: argparse.Namespace):
+    """
+    Main program
+    :param args: arguments from CLI
+    :return: None
+    """
 
-    for instance in arguments.instances:
+    for instance in args.instances:
         try:
-            EC2Cryptomatic(arguments.region, instance, arguments.key).start_encryption(arguments.discard_source)
+            EC2Cryptomatic(args.region,
+                           instance,
+                           args.key).start_encryption(args.discard_source)
 
         except (EndpointConnectionError, ValueError) as error:
             logger.error('Problem with your AWS region ? (%s)' % error)
@@ -212,7 +211,10 @@ def main(arguments):
             continue
 
 
-if __name__ == '__main__':
+def parse_arguments() -> argparse.Namespace:
+    """
+    Parse arguments from CLI
+    """
     description = 'EC2Cryptomatic - Encrypt EBS volumes from EC2 instances'
     parser = argparse.ArgumentParser(description=description)
     parser.add_argument('-r', '--region', help='AWS Region', required=True)
@@ -221,5 +223,8 @@ if __name__ == '__main__':
     parser.add_argument('-k', '--key', help="KMS Key ID. For alias, add prefix 'alias/'", default='alias/aws/ebs')
     parser.add_argument('-ds', '--discard_source', action='store_true', default=False,
                         help='Discard source volume after encryption (default: False)')
-    args = parser.parse_args()
-    main(args)
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    main(parse_arguments())
